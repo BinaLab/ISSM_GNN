aeafeb68c356dcb710f1fcd4e2c81cfcbd1b4729
YoungHyunKoo <kooala317@gmail.com>
2023-10-13 00:56:35 -0400

    updates with DGL library

---
+++ DGL_model.py
@@ -0,0 +1,170 @@
+import numpy as np
+
+import torch    
+import torch.nn as nn
+import torch.nn.functional as F
+
+from dgl.nn.pytorch import GINConv, SumPooling    
+from dgl.nn import GraphConv
+
+
+## Graph Isomorphism Network (GIN) ========================
+class GIN(nn.Module):
+    def __init__(self, ch_input, ch_output, hidden_channels = 128:
+        super(GIN, self).__init__()
+        
+        self.activation = nn.ReLU()
+        self.conv1 = GINConv(nn.Linear(ch_input, hidden_channels), aggregator_type='sum')
+        self.conv2 = GINConv(nn.Linear(hidden_channels, ch_output), aggregator_type='sum')
+        self.pool = SumPooling()
+
+    def forward(self, g, feats):
+        feats = self.conv1(g, feats)
+        feats = self.activation(feats)
+        feats = self.conv2(g, feats)
+
+        return self.pool(g, feats)
+
+## Multi-layer perceptron ===============================
+class MLP(nn.Module):
+    
+    def __init__(self, ch_input, ch_output, hidden_channels = 128):
+        super(MLP, self).__init__()
+        # torch.manual_seed(1234567)
+        self.activation = nn.ReLU() #nn.LeakyReLU(negative_slope=0.01) #nn.Tanh()
+        
+        self.lin1 = torch.nn.Linear(ch_input, hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin3 = torch.nn.Linear(hidden_channels, ch_output)
+
+    def forward(self, g, in_feat):
+        
+        x = self.activation(self.lin1(in_feat));
+        x = self.activation(self.lin2(x));
+        x = self.lin3(x);
+        
+        return x
+    
+## Graph convolutional network =============================
+class GCN(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GraphConv(in_feats, h_feats)
+        self.conv2 = GraphConv(h_feats, num_classes)
+    
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.relu(h)
+        h = self.conv2(g, h)
+        return h
+
+    
+class FCNet(torch.nn.Module):
+    def __init__(self, ch_input, ch_output, hidden_channels = 128):
+        super().__init__()
+        # torch.manual_seed(1234567)
+        self.activation = nn.LeakyReLU(negative_slope=0.01) #nn.Tanh()
+        
+        self.lin1 = torch.nn.Linear(ch_input, hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin3 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin4 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin5 = torch.nn.Linear(hidden_channels, ch_output)
+        
+        self.dropout = nn.Dropout(0.0)
+
+    def forward(self, x, pos, edge_index):
+        
+        x = self.dropout(self.activation(self.lin1(x)));
+        x = self.dropout(self.activation(self.lin2(x)));
+        x = self.dropout(self.activation(self.lin3(x)));
+        x = self.activation(self.lin4(x));
+        x = self.lin5(x);
+
+        
+        return x
+    
+
+def index_sum(agg_size, source, idx, cuda):
+    """
+        source is N x hid_dim [float]
+        idx    is N           [int]
+        
+        Sums the rows source[.] with the same idx[.];
+    """
+    tmp = torch.zeros((agg_size, source.shape[1]))
+    tmp = tmp.cuda() if cuda else tmp
+    res = torch.index_add(tmp, 0, idx, source)
+    return res
+
+class EGCNet(torch.nn.Module):
+    def __init__(self, ch_input, ch_output, hidden_channels = 128, cuda=True):
+        super().__init__()
+        # torch.manual_seed(1234567)
+        self.activation = nn.LeakyReLU(negative_slope=0.01) #nn.Tanh()
+        # self.emb = nn.Linear(ch_input, hidden_channels) 
+        self.gnn = ConvEGNN(ch_input, hidden_channels, cuda=cuda)
+        # self.conv2 = GCNConv(hidden_channels, hidden_channels, improved=True)
+        
+        self.lin1 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin3 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin4 = torch.nn.Linear(hidden_channels, hidden_channels)
+        self.lin5 = torch.nn.Linear(hidden_channels, ch_output)
+        
+        self.dropout = nn.Dropout(0.0)
+
+    def forward(self, x, pos, edge_index):
+        # x = self.emb(x)
+        x = self.gnn(x, pos, edge_index); #self.conv1(x)
+        x = self.dropout(self.activation(self.lin1(x)));
+        x = self.dropout(self.activation(self.lin2(x)));
+        x = self.dropout(self.activation(self.lin3(x)));
+        x = self.dropout(self.activation(self.lin4(x)));
+        x = self.lin5(x);
+        
+        return x
+    
+class ConvEGNN(nn.Module):
+    def __init__(self, in_dim, hid_dim, cuda=True):
+        super().__init__()
+        self.hid_dim=hid_dim
+        self.cuda = cuda
+        
+        # computes messages based on hidden representations -> [0, 1]
+        self.f_e = nn.Sequential(
+            nn.Linear(in_dim*2+1, hid_dim), nn.Tanh(),
+            nn.Linear(hid_dim, hid_dim), nn.Tanh())
+        
+        # preducts "soft" edges based on messages 
+        self.f_inf = nn.Sequential( 
+            nn.Linear(hid_dim, 1),
+            nn.Tanh())
+        
+        # updates hidden representations -> [0, 1]
+        self.f_h = nn.Sequential(
+            nn.Linear(in_dim+hid_dim, hid_dim), nn.Tanh(),
+            nn.Linear(hid_dim, hid_dim))
+    
+    def forward(self, b, pos, edge_index):
+        e_st = edge_index[0, :]
+        e_end = edge_index[1, :]
+
+        dists = torch.norm(torch.square(pos[e_st] - pos[e_end]), dim=1).reshape(-1, 1)
+        
+        # compute messages
+        tmp = torch.hstack([b[e_st], b[e_end], dists])
+        
+        m_ij = self.f_e(tmp)
+        
+        # predict edges
+        e_ij = self.f_inf(m_ij)
+        
+        # average e_ij-weighted messages  
+        # m_i is num_nodes x hid_dim
+        m_i = index_sum(b.shape[0], e_ij*m_ij, edge_index[0,:], self.cuda)
+        
+        # update hidden representations
+        b += self.f_h(torch.hstack([b, m_i]))
+        del tmp, m_ij, e_ij, m_i
+        return b
---
+++ ISSM_DGL_distributed.py
@@ -0,0 +1,398 @@
+ ### PREDICT ONLY SEA ICE U & V
+
+# Ignore warning
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import math
+from datetime import datetime
+from tqdm import tqdm
+import time
+import pickle
+
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR
+
+import torch.distributed as dist
+from torch.utils import collect_env
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+# from torch_geometric.loader import DataLoader
+ 
+# from torch.utils.tensorboard import SummaryWriter
+
+from DGL_model import *
+
+import argparse
+import os    
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    filepath: str,
+) -> None:
+    """Save model checkpoint."""
+    state = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    torch.save(state, filepath)
+
+
+def parse_args() -> argparse.Namespace:
+    """Get cmd line args."""
+    
+    # General settings
+    parser = argparse.ArgumentParser(description='PyTorch Example')   
+    parser.add_argument(
+        '--model-dir',
+        default='../model',
+        help='Model directory',
+    )
+    parser.add_argument(
+        '--log-dir',
+        default='./logs/torch_unet',
+        help='TensorBoard/checkpoint directory',
+    )
+    parser.add_argument(
+        '--checkpoint-format',
+        default='checkpoint_unet_{epoch}.pth.tar',
+        help='checkpoint file format',
+    )
+    parser.add_argument(
+        '--checkpoint-freq',
+        type=int,
+        default=10,
+        help='epochs between checkpoints',
+    )
+    parser.add_argument(
+        '--no-cuda',
+        # action='store_true',
+        default=False,
+        help='disables CUDA training',
+    )    
+    parser.add_argument(
+        '--seed',
+        type=int,
+        default=0,
+        metavar='S',
+        help='random seed (default: 42)',
+    )
+    
+    # Training settings
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=8,
+        metavar='N',
+        help='input batch size for training (default: 16)',
+    )
+    parser.add_argument(
+        '--batches-per-allreduce',
+        type=int,
+        default=1,
+        help='number of batches processed locally before '
+        'executing allreduce across workers; it multiplies '
+        'total batch size.',
+    )
+    parser.add_argument(
+        '--val-batch-size',
+        type=int,
+        default=8,
+        help='input batch size for validation (default: 16)',
+    )
+    parser.add_argument(
+        '--phy',
+        type=str,
+        default='nophy',
+        help='filename of dataset',
+    )
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=100,
+        metavar='N',
+        help='number of epochs to train (default: 100)',
+    )
+    parser.add_argument(
+        '--base-lr',
+        type=float,
+        default=0.01,
+        metavar='LR',
+        help='base learning rate (default: 0.01)',
+    )
+    parser.add_argument(
+        '--model-type',
+        type=str,
+        default="egcn",
+        help='types of the neural network model (e.g. unet, cnn, fc)',
+    )
+    
+    parser.add_argument(
+        '--backend',
+        type=str,
+        default='nccl',
+        help='backend for distribute training (default: nccl)',
+    )
+    
+    args = parser.parse_args()
+    if 'LOCAL_RANK' in os.environ:
+        args.local_rank = int(os.environ['LOCAL_RANK'])
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    return args
+                
+
+##########################################################################################
+
+import torch.distributed as dist
+
+from dgl import save_graphs, load_graphs
+from dgl.data.utils import makedirs, save_info, load_info
+
+def init_process_group(world_size, rank):
+    dist.init_process_group(
+        backend='gloo',     # change to 'nccl' for multiple GPUs
+        init_method='tcp://127.0.0.1:12345',
+        world_size=world_size,
+        rank=rank)
+
+class ISSMDataset(DGLDataset):
+    def __init__(self, file):
+        super().__init__(name='pig')
+        self.file = file
+        
+    def process(self):
+        glist, _ = load_graphs(self.file)
+        self.graphs = glist
+        
+    def __getitem__(self, i):
+        return self.graphs[i]
+    
+    def __len__(self):
+        return len(self.graphs)
+
+###############################################################################
+# Data Loader Preparation
+# -----------------------
+#
+# We split the dataset into training, validation and test subsets. In dataset
+# splitting, we need to use a same random seed across processes to ensure a
+# same split. We follow the common practice to train with multiple GPUs and
+# evaluate with a single GPU, thus only set `use_ddp` to True in the
+# :func:`~dgl.dataloading.pytorch.GraphDataLoader` for the training set, where 
+# `ddp` stands for :func:`~torch.nn.parallel.DistributedDataParallel`.
+#
+
+from dgl.data import split_dataset
+from dgl.dataloading import GraphDataLoader
+
+def get_dataloaders(dataset, seed, batch_size=32):
+    # Use a 80:10:10 train-val-test split
+    train_set, val_set, test_set = split_dataset(dataset,
+                                                 frac_list=[0.8, 0.15, 0.05],
+                                                 shuffle=True,
+                                                 random_state=seed)
+    train_loader = GraphDataLoader(train_set, use_ddp=True, batch_size=batch_size, shuffle=True)
+    val_loader = GraphDataLoader(val_set, batch_size=batch_size)
+    # test_loader = GraphDataLoader(test_set, batch_size=batch_size)
+
+    return train_loader, val_loader #, test_loader
+
+###############################################################################
+# To ensure same initial model parameters across processes, we need to set the
+# same random seed before model initialization. Once we construct a model
+# instance, we wrap it with :func:`~torch.nn.parallel.DistributedDataParallel`.
+#
+
+import torch
+from torch.nn.parallel import DistributedDataParallel
+
+def init_model(seed, device):
+    torch.manual_seed(seed)
+    model = GIN().to(device)
+    if device.type == 'cpu':
+        model = DistributedDataParallel(model)
+    else:
+        model = DistributedDataParallel(model, device_ids=[device], output_device=device)
+
+    return model
+
+###############################################################################
+# Main Function for Each Process
+# -----------------------------
+#
+# Define the model evaluation function as in the single-GPU setting.
+#
+
+def evaluate(model, dataloader, device):
+    model.eval()
+
+    total = 0
+    total_correct = 0
+
+    for bg in dataloader:
+        bg = bg.to(device)
+        feats = bg.ndata['feat']
+        labels = bg.ndata['labels']
+        with torch.no_grad():
+            pred = model(bg, feats)
+        _, pred = torch.max(pred, 1)
+        total += len(labels)
+        total_correct += (pred == labels).sum().cpu().item()
+
+    return 1.0 * total_correct / total
+
+###############################################################################
+# Define the main function for each process.
+#
+
+from torch.optim import Adam
+
+def main(rank, world_size, train_set, seed=0):
+    
+    args = parse_args()
+    
+    torch.distributed.init_process_group(
+        backend=args.backend,
+        init_method='env://',
+        world_size=world_size,
+        rank=rank
+    )
+    
+    model_dir = args.model_dir   
+
+    n_epochs = args.epochs
+    batch_size = args.batch_size  # size of each batch
+    val_batch = args.val_batch_size  # size of validation batch size
+    lr = args.base_lr
+
+    phy = args.phy ## PHYSICS OR NOT
+    
+    if args.no_cuda:
+        device = torch.device('cpu')
+    else:
+        device = torch.device('cuda:{:d}'.format(rank))
+        torch.cuda.set_device(device)  
+        
+    print("######## TRAINING/VALIDATION DATA IS PREPARED ########")
+    
+    torch.cuda.empty_cache()
+    
+    n_nodes = 1112
+    in_channels = 4
+    out_channels = 3
+    
+    if args.model_type == "gcn":
+        model = GCN(in_channels, out_channels, 128)  # Graph convolutional network    
+    elif args.model_type == "gin":
+        model = GIN(in_channels, out_channels, 128, cuda)  # Equivariant Graph convolutional network
+    elif args.model_type == "mlp":
+        model = MLP(in_channels, out_channels, 128)  # Fully connected network
+    
+    model_name = f"torch_dgl_{args.model_type}_lr{lr}_{phy}_{device_name}_v2_ch{out_channels}"
+    
+    torch.manual_seed(seed)
+
+    if args.no_cuda:
+        model = DistributedDataParallel(model)
+    else:
+        model = DistributedDataParallel(model, device_ids=[device], output_device=device)
+    
+    criterion = nn.MSELoss() #nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lrlr)    
+    
+    train_loader, val_loader = get_dataloaders(train_set, seed)
+    
+    for epoch in range(5):
+        model.train()
+        # The line below ensures all processes use a different
+        # random ordering in data loading for each epoch.
+        train_loader.set_epoch(epoch)
+        
+        ##### TRAIN ###########################
+        train_loss = 0
+        train_count = 0
+        for bg in train_loader:
+            bg = bg.to(device)
+            feats = bg.ndata['feat']
+            if out_channel == 6:
+                labels = bg.ndata['labels']
+            elif out_channel == 3:
+                labels = bg.ndata['labels'][: [1,2,4]]            
+            pred = model(bg, feats)
+
+            loss = criterion(pred, labels)
+            train_loss += loss.cpu().item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_count += 1       
+        
+        ##### VALIDATION ######################
+        val_loss = 0
+        val_count = 0
+        for bg in val_loader:
+            bg = bg.to(device)
+            feats = bg.ndata['feat']
+            if out_channel == 6:
+                labels = bg.ndata['labels']
+            elif out_channel == 3:
+                labels = bg.ndata['labels'][: [1,2,4]]
+            
+            with torch.no_grad():
+                pred = model(bg, feats)
+            loss = criterion(pred, labels)
+            val_loss += loss.cpu().item()
+            val_count += 1
+            
+        print('Train loss: {0:.4f}; Val loss: {1:.4f}'.format(train_loss / train_count, val_loss / val_count))
+        
+    if rank == 0:
+        test_set = ISSMDataset("../data/DGL_test_dataset.bin")
+        ##### TEST ########################
+        rates = np.zeros(len(test_set))
+        years = np.zeros(len(test_set))
+
+        if out_channel == 6:
+            scaling = [1, 5000, 5000, 5000, 4000]
+        elif out_channel == 3:
+            scaling = [5000, 5000, 4000]
+        y_pred = np.zeros([len(test_set), n_nodes, out_channel])
+        y_true = np.zeros([len(test_set), n_nodes, out_channel])
+
+        for k, bg in enumerate(test_set):
+            bg = bg.to(device)
+            feats = bg.ndata['feat']
+            if out_channel == 6:
+                labels = bg.ndata['labels']
+            elif out_channel == 3:
+                labels = bg.ndata['labels'][: [1,2,4]] 
+
+            with torch.no_grad():
+                pred = model(bg, feats)  
+                y_pred[k] = pred
+                y_true[k] = labels
+
+        test_save = [rates, years, y_true.to('cpu').detach().numpy(), y_pred.to('cpu').detach().numpy()]
+
+        with open(f'../results/test_{model_name}.pkl', 'wb') as file:
+            pickle.dump(test_save, file)
+
+    dist.destroy_process_group()
+
+###############################################################################
+if __name__ == '__main__':
+    import torch.multiprocessing as mp
+
+    num_gpus = 4
+    procs = []
+    train_set = ISSMDataset("../data/DGL_train_dataset.bin")    
+    mp.spawn(main, args=(num_gpus, train_set), nprocs=num_gpus)
+
