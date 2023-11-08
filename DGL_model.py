1113df271b40d9ccdd0dba423612b5db5d235cfa
YoungHyunKoo <kooala317@gmail.com>
2023-11-08 18:12:32 -0500

    updates on GCN

--- DGL_model.py
+++ DGL_model.py
@@ -98,7 +98,7 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
         self.activation = nn.LeakyReLU() #nn.LeakyReLU() #nn.ReLU() #nn.LeakyReLU(negative_slope=0.01) #nn.Tanh()
         self.conv1 = GraphConv(in_feats, h_feats)
-        # self.conv2 = GraphConv(h_feats, h_feats)
+        self.conv2 = GraphConv(h_feats, h_feats)
         self.lin1 = torch.nn.Linear(h_feats, h_feats)
         self.lin2 = torch.nn.Linear(h_feats, h_feats)
         self.lin3 = torch.nn.Linear(h_feats, h_feats)
@@ -108,7 +108,7 @@ class GCN(nn.Module):
     
     def forward(self, g, in_feat):
         h = self.activation(self.conv1(g, in_feat))
-        # h = self.activation(self.conv2(g, h))
+        h = self.activation(self.conv2(g, h))
         h = self.activation(self.lin1(h));
         h = self.activation(self.lin2(h));
         h = self.activation(self.lin3(h));
