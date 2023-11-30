ef84ebcace48a33883926afd79c0453fdbb64fff
YoungHyunKoo <kooala317@gmail.com>
2023-11-30 17:25:57 -0500

    add FCN

--- ISSM_FCN_distributed.py
+++ ISSM_FCN_distributed.py
@@ -530,7 +530,7 @@ def main():
             with torch.no_grad():
                 pred = model(data, sampling)
                 for n in range(0, n_nodes):
-                    y_pred[k] = pred[:, :out_channels, sampling[n][0], sampling[n][1]]
+                    y_pred[k] = pred[:, :out_channels, sampling[n][0], sampling[n][1]].cpu()
                 # y_pred[k] = pred[0, :, :out_channels].to('cpu')
                 y_true[k] = target[0, :, :out_channels].to('cpu')
                 x_inputs[k] = test_graphs[k].ndata['feat'][:, :-1].to('cpu')
