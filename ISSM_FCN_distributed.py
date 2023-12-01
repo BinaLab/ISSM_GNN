ec7415e6e012c375180fc5f2c21ab92ad6101bd3
YoungHyunKoo <kooala317@gmail.com>
2023-12-01 14:01:50 -0500

    add FCN

--- ISSM_FCN_distributed.py
+++ ISSM_FCN_distributed.py
@@ -532,6 +532,7 @@ def main():
                 for n in range(0, n_nodes):
                     y_pred[k] = pred[:, :out_channels, sampling[n][0], sampling[n][1]].cpu()
                 # y_pred[k] = pred[0, :, :out_channels].to('cpu')
+                print(np.nanmean(y_pred[k][0]), np.nanmean(y_pred[k][1]), np.nanmean(y_pred[k][2]))
                 y_true[k] = test_graphs[k].ndata['label'][:, :out_channels].to('cpu')
                 x_inputs[k] = test_graphs[k].ndata['feat'][:, :-1].to('cpu')
 
