a55062848fc91829ad2ce677232e85bb3df78ff6
YoungHyunKoo <kooala317@gmail.com>
2023-12-01 14:28:05 -0500

    add CNN model

--- ISSM_FCN_distributed.py
+++ ISSM_FCN_distributed.py
@@ -503,7 +503,6 @@ def main():
         
     if args.local_rank == 0:
         ##### TEST ########################
-        print(sampling)
         rates = np.zeros(len(test_dataset))
         years = np.zeros(len(test_dataset))
 
