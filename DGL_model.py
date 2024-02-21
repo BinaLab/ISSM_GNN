cf3f3f753ec28c140b5eb415712ea00aaeec9e3b
YoungHyunKoo <kooala317@gmail.com>
2024-02-21 16:27:35 -0500

    add NO

--- DGL_model.py
+++ DGL_model.py
@@ -610,7 +610,7 @@ class EGKN(torch.nn.Module):
 
     def forward(self, g, in_feat):
         h = in_feat
-        edge_index = torch.cat([g.edges()[0][:, None], g.edges()[1][:, None]], axis = 1)
+        edge_index = torch.cat([g.edges()[0][None, :], g.edges()[1][None, :]], axis = 0)
         coords_curr = g.ndata['feat'][:, :2].detach().clone()
         edge_attr = torch.cat([g.edata['weight'][0][:, None], g.edata['slope'][0][:, None]], axis = 1)
 
