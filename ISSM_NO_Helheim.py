d8bd3ddb8f42257f6f3f675ee1441797f2a6aaed
YoungHyunKoo <kooala317@gmail.com>
2024-02-21 17:25:52 -0500

    add NO

--- ISSM_NO_Helheim.py
+++ ISSM_NO_Helheim.py
@@ -384,7 +384,7 @@ def main():
         model = ChebGCN(in_channels, out_channels, 128)  # Equivariant Graph convolutional network
     elif args.model_type == "ino":
         width = 64
-        ker_width = 256
+        ker_width = 128
         edge_features = 2
         n_layer = 2 * 2
         model = EGKN(width, ker_width, n_layer, edge_features, in_channels, out_channels).to(device)
