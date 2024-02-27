66f14da46a3dfd8c91e585331d9d96d7cb35648c
YoungHyunKoo <kooala317@gmail.com>
2024-02-27 11:16:29 -0500

    update EGCN

--- ISSM_DGL_Helheim.py
+++ ISSM_DGL_Helheim.py
@@ -346,7 +346,7 @@ def main():
     train_loader, val_loader = get_dataloaders(train_set, seed, batch_size, True)
     n_nodes = val_set[0].num_nodes()
     in_channels = val_set[0].ndata['feat'].shape[1] - 2 #-1
-    edge_feat_size = val_set[0].edata['weight'].shape[1]
+    edge_feat_size = val_set[0].edata['slope'].shape[1]
     if args.out_ch > 0:
         out_channels = args.out_ch
     else:
@@ -363,7 +363,7 @@ def main():
     # =============================================================
     
     if args.local_rank == 0:
-        print(f"## NODE: {n_nodes}; IN: {in_channels}; OUT: {out_channels}")
+        print(f"## NODE: {n_nodes}; IN: {in_channels}; OUT: {out_channels}; EDGE FEATURES: {edge_feat_size}")
         print(f"## Total: {len(train_set)}; Train: {len(train_loader)*batch_size*world_size}; Val: {len(val_loader)*batch_size*world_size}; Test: {len(val_set)}")
         print("######## TRAINING/VALIDATION DATA IS PREPARED ########")   
     
