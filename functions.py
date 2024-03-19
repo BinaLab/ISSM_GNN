8f85e6604769abb1c56b70273362c4b120646ad4
YoungHyunKoo <kooala317@gmail.com>
2024-03-19 15:34:19 -0400

    FCN update for PIG

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 import scipy.io as sio
 from scipy.interpolate import griddata
+import pickle
 
 import dgl
 from dgl.data import DGLDataset
@@ -135,14 +136,17 @@ class GNN_Helheim_Dataset(DGLDataset):
     def __len__(self):
         return len(self.graphs)
     
-def generate_list(region = "Helheim", folder = "../data"):
+def generate_list(region = "Helheim", folder = "../data", model = "gnn"):
     ## MAKE TRAINING AND TESTING DATASETS FOR GNN
     train_files = []
     val_files = []
     test_files = []
     
     if region == "Helheim":
-        filelist = glob.glob(f'{folder}/Helheim_r*_030.mat')
+        if model == "gnn":
+            filelist = glob.glob(f'{folder}/Helheim_r*_030.mat')
+        elif model == "cnn":
+            filelist = glob.glob(f'{folder}/Helheim_r*_030.mat')
         for f in sorted(filelist):
             rate = f.split("_r")[1][:3]
             if int(rate) <= 100 and rate != "080":
@@ -156,7 +160,10 @@ def generate_list(region = "Helheim", folder = "../data"):
                     train_files.append(f)
                     
     elif region == "PIG":
-        filelist = glob.glob(f'{folder}/PIG_transient_m*_r*.mat')
+        if model == "gnn":
+            filelist = glob.glob(f'{folder}/PIG_transient_m*_r*.mat')
+        elif model == "cnn":
+            filelist = glob.glob(f'{folder}/PIG_transient_m*_r*_CNN.pkl')
         for f in sorted(filelist):
             rate = int(f.split("_r")[1][:3])
             if rate % 20 == 0:
@@ -294,85 +301,33 @@ class GNN_PIG_Dataset(DGLDataset):
 
 class CNN_PIG_Dataset(Dataset):
     def __init__(self, files):
-        self.input = []
-        self.output = []
         
-        # # Region filtering
-        # filename = f'D:\\ISSM\\Helheim\\Helheim_r100_030.mat'
-        # test = sio.loadmat(filename)
-        # mask = test['S'][0][0][11][0]
+        self.input = torch.tensor([])
+        self.output = torch.tensor([])
 
         first = True
         # "READING GRAPH DATA..."
         for filename in tqdm(files[:]):
+            
+            with open(filename, 'rb') as file:
+                [input0, output0] = pickle.load(file)
+            
             rate = int(filename.split("_r")[1][:3])
-            test = sio.loadmat(filename)
-
-            xc = test['S'][0][0][0]
-            yc = test['S'][0][0][1]
-            elements = test['S'][0][0][2]-1
-            smb = test['S'][0][0][3]
-            vx = test['S'][0][0][4]
-            vy = test['S'][0][0][5]
-            vel = test['S'][0][0][6]
-            surface = test['S'][0][0][7]
-            base = test['S'][0][0][8]
-            H = test['S'][0][0][9]
-            f = test['S'][0][0][10]
-            # mask = test['S'][0][0][11]
-            # ice = np.zeros(mask.shape) # Negative: ice; Positive: no-ice
-            # ice[mask > 0] = 0.5 # ice = 0; no-ice = 1
-            # ice = np.where(mask < 0, mask / 1000000, mask/10000)
-
-            n_year, n_sample = H.shape        
+            input0 = torch.tensor(input0)
+            output0 = torch.tensor(output0)
             
-            coord = np.array([xc[:, 0], yc[:, 0]]).transpose()
-            gridx, gridy = np.meshgrid(np.arange(xc.min(), xc.max(), 1000), np.arange(yc.min(), yc.max(), 1000))
-            input0 = np.zeros((n_year, 12, gridx.shape[0], gridx.shape[1]))
-            output0 = np.zeros((n_year, 6, gridx.shape[0], gridx.shape[1]))          
-            
-            for t in range(0, n_year):
-                
-                # INPUT: x/y coordinates, melting rate, time, SMB, Vx0, Vy0, Surface0, Base0, Thickness0, Floating0
-                inputs = torch.zeros([n_sample, 12])
-                # OUTPUT: Vx, Vy, Vel, Surface, Thickness, Floating
-                outputs = torch.zeros([n_sample, 6])
-
-                ## INPUTS ================================================
-                inputs[:, 0] = torch.tensor((xc[:, 0]-xc.min())/10000) # torch.tensor(xc[0, :]/10000) # torch.tensor((xc[:, 0]-xc.min())/(xc.max()-xc.min())) # X coordinate
-                inputs[:, 1] = torch.tensor((yc[:, 0]-yc.min())/10000) # torch.tensor(yc[0, :]/10000) # torch.tensor((yc[:, 0]-yc.min())/(yc.max()-yc.min())) # Y coordinate
-                inputs[:, 2] = torch.where(torch.tensor(f[0, :]) < 0, rate/100, 0) # Melting rate (0-100)
-                inputs[:, 3] = torch.tensor(t/n_year) # Year
-                inputs[:, 4] = torch.tensor(smb[t, :]/20) # Surface mass balance
-                inputs[:, 5] = torch.tensor(vx[0, :]/10000) # Initial Vx
-                inputs[:, 6] = torch.tensor(vy[0, :]/10000) # Initial Vx
-                inputs[:, 7] = torch.tensor(vel[0, :]/10000) # Initial Vel
-                inputs[:, 8] = torch.tensor(surface[0, :]/5000) # Initial surface elevation
-                inputs[:, 9] = torch.tensor(base[0, :]/5000) # Initial base elevation
-                inputs[:, 10] = torch.tensor(H[0, :]/5000) # Initial ice thickness
-                inputs[:, 11] = torch.tensor(f[0, :]/5000) # Initial floating part
-                # inputs[:, 11] = torch.tensor(ice[0, :]) # Initial ice mask
-
-                ## OUTPUTS ===============================================
-                outputs[:, 0] = torch.tensor(vx[t, :]/10000) # Initial Vx
-                outputs[:, 1] =  torch.tensor(vy[t, :]/10000) # Initial Vx
-                outputs[:, 2] = torch.tensor(vel[t, :]/10000) # Initial surface elevation
-                outputs[:, 3] = torch.tensor(surface[t, :]/5000) # Initial base elevation
-                outputs[:, 4] = torch.tensor(H[t, :]/5000) # Initial ice thickness
-                outputs[:, 5] = torch.tensor(f[t, :]/5000) # Initial floating part 
-                # outputs[:, 5] = torch.tensor(ice[t, :]) # Initial floating part 
-
-                for c in range(0, inputs.shape[1]):
-                    input0[t, c, :, :] = griddata(coord, inputs[:, c], (gridx, gridy), method='linear')
-                    output0[t, c, :, :] = griddata(coord, outputs[:, c], (gridx, gridy), method='linear')
-                
-            self.input.append(input0)
-            self.output.append(output0)
+            if first:
+                self.input = input0
+                self.output = output0
+                first = False
+            else:
+                self.input = torch.cat((self.input, input0), dim = 0)
+                self.output = torch.cat((self.output, output0), dim = 0)
         
     def __getitem__(self, i):
-        cnn_input = torch.tensor(self.input[n], dtype=torch.float32)
+        cnn_input = self.input[i]
         cnn_input[torch.isnan(cnn_input)] = 0        
-        cnn_output = torch.tensor(self.output[n], dtype=torch.float32)
+        cnn_output = self.output[i]
         cnn_output[torch.isnan(cnn_output)] = 0
         return (cnn_input, cnn_output)
     
