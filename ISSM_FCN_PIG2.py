628d4db9437931721b864db9bc036ce2a66b2cfc
YoungHyunKoo <kooala317@gmail.com>
2024-03-20 02:13:02 -0400

    FCN update for PIG

--- ISSM_FCN_PIG2.py
+++ ISSM_FCN_PIG2.py
@@ -374,7 +374,7 @@ def main():
     mesh = args.mesh
     
     train_files, val_files, test_files = generate_list(region = "PIG", model = "cnn")
-    train_dataset = CNN_PIG_Dataset(train_files[:10])
+    train_dataset = CNN_PIG_Dataset(train_files[:])
     val_dataset = CNN_PIG_Dataset(val_files[:])
     test_dataset = CNN_PIG_Dataset(test_files[:])
     
