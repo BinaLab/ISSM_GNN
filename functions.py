c15ace704e7b1d7aadf8af073e84ea83668d0ce6
YoungHyunKoo <kooala317@gmail.com>
2025-01-20 20:25:36 -0700

    Add flow model experiments (01/20/2025)

--- functions.py
+++ functions.py
@@ -236,7 +236,7 @@ def generate_list(region = "Helheim", folder = "../data", train = [], model = "g
             filelist = glob.glob(f'{folder}/Helheim_r*_030_CNN_200m.pkl')
         for f in sorted(filelist):
             rate = f.split("_r")[1][:3]
-            if int(rate) <= 110:
+            if int(rate) <= 110 and int(rate) >= 70:
                 if rate in train:
                     train_files.append(f)
                 else:
