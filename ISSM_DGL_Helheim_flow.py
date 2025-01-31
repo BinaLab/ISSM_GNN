10f6999e2f002c53f23a2dcedf99e87598fd26e0
YoungHyunKoo <kooala317@gmail.com>
2025-01-31 15:45:02 -0700

    Helheim experiments (1/31/2025)

--- ISSM_DGL_Helheim_flow.py
+++ ISSM_DGL_Helheim_flow.py
@@ -372,13 +372,6 @@ def main():
             else:
                 mask_batch = np.append(mask_batch, mask+i*mask.shape[0])
         # =============================================================
-        
-        if args.local_rank == 0:
-            print("\n############################################################")
-            print("## Train list: ", train_num, train_list)
-            print(f"## NODES: {n_nodes}; EDGES: {n_edges}; IN: {in_channels}; OUT: {out_channels}; EDGE FEATURES: {edge_feat_size}")
-            print(f"## Total: {len(train_set)}; Train: {len(train_loader)*batch_size*world_size}; Val: {len(val_loader)*batch_size*world_size}; Test: {len(val_set)}")
-            print("########### TRAINING/VALIDATION DATA IS PREPARED #############")   
 
         hidden_channels = args.hidden_ch
         
@@ -387,6 +380,13 @@ def main():
         elif args.initial == "flowt":
             model_name = f"torch_dgl_HelheimFLOWT_{args.model_type}_{n_nodes}_train{train_num}_lr{lr}_in{in_channels}_ch{out_channels}_ft{hidden_channels}_gpu{world_size}"
             in_channnels = 7; out_channels = 4
+
+        if args.local_rank == 0:
+            print("\n############################################################")
+            print("## Train list: ", train_num, train_list)
+            print(f"## NODES: {n_nodes}; EDGES: {n_edges}; IN: {in_channels}; OUT: {out_channels}; EDGE FEATURES: {edge_feat_size}")
+            print(f"## Total: {len(train_set)}; Train: {len(train_loader)*batch_size*world_size}; Val: {len(val_loader)*batch_size*world_size}; Test: {len(val_set)}")
+            print("########### TRAINING/VALIDATION DATA IS PREPARED #############")          
         
         if args.model_type == "gcn":
             model = GCN(in_channels, out_channels, hidden_channels)  # Graph convolutional network    
